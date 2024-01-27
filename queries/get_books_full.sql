fd3cab3ba16602519adf805491f6ee2a9ed8fedf
Bruno Tomas <brunottomas@gmail.com>
2024-01-26 22:39:51 -0300

    fix query

--- queries/get_books_full.sql
+++ queries/get_books_full.sql
@@ -2,5 +2,4 @@ select b.id as book_id, b.name as book_name, a.name as author_name, c.name as co
 join author a
 on b.author_id = a.id
 join country c
-on c.id = a.country_id
-where inserted_at > %s;
+on c.id = a.country_id;
